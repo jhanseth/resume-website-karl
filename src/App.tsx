e1d3283eb0b063a52cfda7c390407370ac069bb5
jhanseth <72149144+jhanseth@users.noreply.github.com>
2023-09-25 19:14:37 +0200

    Initialized

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+<h1 className="text-3xl font-bold underline" >Karl Hanseth</h1>
+<p>Technical Director</p>
+<p>Erfaren leder og teknisk direktør med solid endrings- og innovasjonskompetanse fra ulike bransjer. Sterk på strategi, organisasjonsforståelse og bærekraft. Har en operativ og relasjonsorientert tilnærming i min lederstil, samtidig med at jeg liker å jobbe målrettet, handlekraftig og proaktivt.</p>
+    </>
+  )
+}
+
+export default App
