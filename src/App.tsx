c881707771d453c7ac2a65fa0d383f9dc466f062
jhanseth <72149144+jhanseth@users.noreply.github.com>
2023-09-30 11:25:27 +0200

    Fix mobile

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import karlOpera from "./assets/karlOperaen.jpg";
 import karlHeadshot from "./assets/karlHeadshot.jpg";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import DownloadIcon from "@mui/icons-material/Download";
+import EmailIcon from "@mui/icons-material/Email";
+import PhoneIphoneIcon from "@mui/icons-material/PhoneIphone";
+import CorporateFareIcon from "@mui/icons-material/CorporateFare";
+import HomeIcon from "@mui/icons-material/Home";
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
@@ -16,12 +20,11 @@ function App() {
           <h1>Karl Hanseth</h1>
           <h3>Technical Director</h3>
           <p>
-            Experienced leader and technical director with strong change and
-            innovation expertise from various industries. Proficient in
-            strategy, organizational understanding, and sustainability.
-            Operational and relationship-oriented approach in my leadership
-            style while also enjoying working in a goal-oriented, decisive, and
-            proactive manner.
+            Experienced leader with strong change and innovation expertise from
+            various industries. Proficient in strategy, organizational
+            understanding, and sustainability. Operational and
+            relationship-oriented approach in my leadership style while also
+            enjoying working in a goal-oriented, decisive, and proactive manner.
           </p>
           <h4>Diverse leadership experience from: </h4>
           <p>The Norwegian Opera and Ballet </p>
@@ -29,7 +32,21 @@ function App() {
           <p>Microsoft </p>
           <p>Scandinavian Airlines </p>
           <p>Norwegian Armed Forces</p>
-
+          <h4>Contact: </h4>
+          <div className="contact">
+            <p>
+              <EmailIcon></EmailIcon>karlhanseth@gmail.com
+            </p>
+            <p>
+              <PhoneIphoneIcon></PhoneIphoneIcon>91 91 73 76
+            </p>
+            <p>
+              <HomeIcon></HomeIcon> Jacob Aalls gate 31a 0364 Oslo
+            </p>
+            <p>
+              <CorporateFareIcon></CorporateFareIcon>orgnr 932 068 397
+            </p>
+          </div>
           <div className="buttonSection"></div>
           <Stack direction="row" spacing={2}>
             <Button
