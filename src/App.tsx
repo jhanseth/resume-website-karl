ca06d7302c8a47de968ee922ceceb3e0606d0abf
jhanseth <72149144+jhanseth@users.noreply.github.com>
2023-10-03 20:28:19 +0200

    Styling changes

--- src/App.tsx
+++ src/App.tsx
@@ -26,22 +26,25 @@ function App() {
             relationship-oriented approach in my leadership style while also
             enjoying working in a goal-oriented, decisive, and proactive manner.
           </p>
-          <h4>Diverse leadership experience from: </h4>
-          <p>The Norwegian Opera & Ballet </p>
-          <p>Riksteatret - National touring theater </p>
-          <p>Microsoft </p>
-          <p>Scandinavian Airlines System </p>
-          <p>Norwegian Armed Forces</p>
+          <div className="experienceSection">
+            <h4>Diverse leadership experience from: </h4>
+            <p>- The Norwegian Opera & Ballet </p>
+            <p>- Riksteatret - National touring theater </p>
+            <p>- Microsoft </p>
+            <p>- Scandinavian Airlines System </p>
+            <p>- Norwegian Armed Forces</p>
+          </div>
           <h4>Contact: </h4>
+
           <div className="contact">
             <p>
               <EmailIcon></EmailIcon>karlhanseth@gmail.com
             </p>
             <p>
-              <PhoneIphoneIcon></PhoneIphoneIcon>91 91 73 76
+              <PhoneIphoneIcon></PhoneIphoneIcon>+47 91 91 73 76
             </p>
             <p>
-              <HomeIcon></HomeIcon> Jacob Aalls gate 31a 0364 Oslo
+              <HomeIcon></HomeIcon>Jacob Aalls gate 31a 0364 Oslo
             </p>
             <p>
               <CorporateFareIcon></CorporateFareIcon>orgnr 932 068 397
